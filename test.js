01abe999b58caf01551d0ef45a6aeb4d1ee25076
Linus G Thiel <linus@yesbabyyes.se>
2023-03-07 22:17:15 +0100

    Update doctest function signature, only require file name under test (#4)
    * Update doctest function signature, only require file name under test
    
    - Instead of taking the `t` test context as a parameter, take just
      the file name and return a function taking the test context to
      simplify usage.
    - Update README with the full spec output

--- test.js
+++ test.js
@@ -4,34 +4,34 @@ Deno.test("Doctest", async (t) => {
   await t.step({
     name: "mod",
     ignore: true,
-    fn: (t) => doctest(t, "mod.js"),
+    fn: doctest("mod.js"),
   });
-  await doctest(t, "lib.js");
+  await doctest("lib.js")(t);
 });
 
 Deno.test("Specification", async (t) => {
   await t.step(
     "Functions returning a Promise can be tested like a regular function",
-    (t) => doctest(t, "spec/returns-promise.js"),
+    doctest("spec/returns-promise.js"),
   );
   await t.step(
     "Functions throwing an Error can be tested by expecting a thrown Error",
-    (t) => doctest(t, "spec/expect-error.js"),
+    doctest("spec/expect-error.js"),
   );
   await t.step(
     "Generator functions can be tested in different ways ",
-    (t) => doctest(t, "spec/generator-function.js"),
+    doctest("spec/generator-function.js"),
   );
   await t.step(
     "Examples without a result are not tested",
-    (t) => doctest(t, "spec/example-without-result.js"),
+    doctest("spec/example-without-result.js"),
   );
   await t.step(
     "Sample passing module (@supabase/doctest-js)",
-    (t) => doctest(t, "spec/sample-passing-module.js"),
+    doctest("spec/sample-passing-module.js"),
   );
   await t.step(
     "Sample passing class (@supabase/doctest-js)",
-    (t) => doctest(t, "spec/sample-passing-class.js"),
+    doctest("spec/sample-passing-class.js"),
   );
 });
